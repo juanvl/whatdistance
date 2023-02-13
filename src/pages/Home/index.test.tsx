22233c1612a83fdf53306c0b520247ce9aba8e4b
Juan Victor Leite <jnvleite@gmail.com>
2023-02-13 18:12:11 -0300

    wip tests with vitest

--- src/pages/Home/index.test.tsx
+++ src/pages/Home/index.test.tsx
@@ -1,53 +1,67 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import { Home } from '.';
 
-it('should render', () => {
-  render(<Home />);
-});
-
-it('should be able to submit a valid form', async () => {
-  render(<Home />);
-
-  const cityOfOrigin = screen.getByRole('combobox', {
-    name: /city of origin\*/i,
-  });
-  const cityOfDestination = screen.getByRole('combobox', {
-    name: /city of destination\*/i,
-  });
-  const dateOfTheTrip = screen.getByLabelText(/date of the trip\*/i);
-  const numberOfPassengers = screen.getByRole('spinbutton', {
-    name: /number of passengers\*/i,
-  });
-  const submitButton = screen.getByRole('button', {
-    name: /search/i,
+describe('Home page', () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
   });
 
-  await userEvent.type(cityOfOrigin, 'Paris');
-  await userEvent.type(cityOfDestination, 'Paris');
-  await userEvent.type(dateOfTheTrip, '12/12/2023');
-  await userEvent.type(numberOfPassengers, '3');
-  await userEvent.click(submitButton);
+  it('should render', () => {
+    const text = screen.getByText('City of origin');
+    const button = screen.getByText('Search');
 
-  expect(document).toContain('Your trip');
-});
+    expect(text).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
 
-it('should NOT be able to submit a invalid form', () => {
-  render(<Home />);
-});
+  it('should be able to fill inputs correctly', async () => {
+    const cityOfOrigin = screen.getByRole('combobox', {
+      name: /city of origin\*/i,
+    });
+    const cityOfDestination = screen.getByRole('combobox', {
+      name: /city of destination\*/i,
+    });
+    const dateOfTheTrip = screen.getByLabelText(/date of the trip\*/i);
+    const numberOfPassengers = screen.getByRole('spinbutton', {
+      name: /number of passengers\*/i,
+    });
+    // const submitButton = screen.getByRole('button', {
+    //   name: /search/i,
+    // });
 
-it('should be able to add multiple intermediate cities', () => {
-  render(<Home />);
-});
+    fireEvent.input(cityOfOrigin, 'Paris');
+    fireEvent.input(cityOfDestination, 'Nantes');
+    fireEvent.input(dateOfTheTrip, '12/12/2099');
+    fireEvent.input(numberOfPassengers, '3');
+  });
 
-it('should be able to submit form with multiple intermediate cities', () => {
-  render(<Home />);
-});
+  // it('should be able to submit a valid form', async () => {
 
-it('should modify the page url parameters on any input change', () => {
-  render(<Home />);
-});
+  // });
 
-it('should fill inputs with data from url parameters', () => {
-  render(<Home />);
+  // it('should NOT be able to submit a invalid form', () => {
+  //   render(<Home />);
+  // });
+
+  // it('should be able to add multiple intermediate cities', () => {
+  //   render(<Home />);
+  // });
+
+  // it('should be able to submit form with multiple intermediate cities', () => {
+  //   render(<Home />);
+  // });
+
+  // it('should modify the page url parameters on any input change', () => {
+  //   render(<Home />);
+  // });
+
+  // it('should fill inputs with data from url parameters', () => {
+  //   render(<Home />);
+  // });
 });
